3af997d083debd318ecac5e83a139e1ba2f1a09f
Mona Lisa <bcardines811@gmail.com>
2022-03-24 16:23:20 -0400

    uppercase

--- script.js
+++ script.js
@@ -45,4 +45,33 @@ var lowerCaseChar = [
     'x', 
     'y', 
     'z'
+];
+
+var upperCaseChars = [
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'H',
+    'I',
+    'J',
+    'K',
+    'L',
+    'M',
+    'N',
+    'O',
+    'P',
+    'Q',
+    'R',
+    'S',
+    'T',
+    'U',
+    'V',
+    'W',
+    'X',
+    'Y',
+    'Z'
 ]
